a57294a75f96d0fb878f54bfd03b5056f746839b
a-k-29 <anna.kujat@io-warnemuende.de>
2025-09-08 15:42:13 +0200

    Update functional_evaluation.py

--- functional_evaluation.py
+++ functional_evaluation.py
@@ -5,31 +5,30 @@
 # Load needed functions
 from functions import *
 
-# Directory containing the TM topic or PCA component x OTU matrices
-directory = 'path/to/your/components/'
+# Defining paths for input and output data
+input_data_dir = 'path/to/your/data/'
+mantel_output = 'path/to/your/data/'
+rank_functional_output = 'path/to/your/data/'
+significance_output = 'path/to/your/data/'
+
+# Path to directory containing the TM topic or PCA component x OTU matrices as .csv's
+directory = 'path/to/your/directory/'
 
 # List to store results
 results = []
 
 # Load the FAPROTAX OTU x Function matrix
-otu_function_matrix = pd.read_csv('path/to/your/function_matrix', index_col=1, header=0)
-
+otu_function_matrix = pd.read_csv(input_data_dir + 'example_otu_function_matrix.csv', index_col=0, header=0)
 # Load full microbiome data and find functional annotated OTUs (as an extra savety step)
-otus_16 = pd.read_csv('path/to/your/data', header=0, sep=',', index_col=0).T
+otus_16 = pd.read_csv(input_data_dir + 'path/example_otu_data.csv', header=0, sep=',', index_col=0).T
 n_ids = otu_function_matrix.index.values.tolist()
 n_ids_16 = otus_16.index.values.tolist()
-# identify the common sample ids of microbiome data and function matrix
+# Identify the common sample ids of microbiome data and function matrix
 common_ids = [x for x in n_ids_16 if x in n_ids]
 # Keep common ids
 otu_function_matrix = otu_function_matrix.loc[common_ids]
 
-# Modify function matrix: Replace strings with integers; remove columns with non-binary categories
-otu_function_matrix = otu_function_matrix.replace('variable', 0.5)
-otu_function_matrix = otu_function_matrix.replace('yes,no', 0.5)
-otu_function_matrix = otu_function_matrix.replace('yes', 1)
-otu_function_matrix = otu_function_matrix.replace('no', 0)
-otu_function_matrix = otu_function_matrix.drop(otu_function_matrix[['elements', 'main_element', 'electron_donor', 'electron_acceptor']], axis=1)
-# Convert to int
+# Convert to int type
 otu_function_matrix = otu_function_matrix.astype(int)
 # Calculate Jaccard distance matrix for the OTU x function matrix
 jaccard_distance_matrix = compute_jaccard_distance(otu_function_matrix)
@@ -41,13 +40,12 @@ for filename in os.listdir(directory):
         tm, dim, prepro = parse_filename(filename)
         # Load the topic or PCA-component x OTU matrix
         filepath = os.path.join(directory, filename)
-        topic_otu_matrix = pd.read_csv(filepath, index_col=0).T
+        topic_otu_matrix = pd.read_csv(filepath, index_col=0)
         # Normalize components
         topic_otu_matrix = topic_otu_matrix.div(topic_otu_matrix.sum(axis=1), axis=0)
-        # Keep the common ids
-        topic_otu_matrix = topic_otu_matrix.loc[common_ids]
         # Compute the Euclidean distance matrix for the topic or PCA Component x OTU matrix
-        euclidean_distance_matrix = compute_euclidean_distance(topic_otu_matrix)
+        # Function 'compute_euclidean_distance' expects OTUs in rows
+        euclidean_distance_matrix = compute_euclidean_distance(topic_otu_matrix.T)
         # Perform Mantel test
         correlation, p_value = mantel_test(jaccard_distance_matrix, euclidean_distance_matrix)
         # Store results
@@ -56,23 +54,22 @@ for filename in os.listdir(directory):
 # Put results in a dataframe
 results_df = pd.DataFrame(results, columns=['tm', 'dim', 'prepro', 'mantel_correlation', 'p_value'])
 results_df = results_df.sort_values(by='mantel_correlation', ascending= False)
+
 # Save results
-#results_df.to_csv('your/output/path', index=False)
+results_df.to_csv(mantel_output + 'mantel_functional_evaluation.csv', index=False)
 
 # Rank the results
-results_df = results_df[results_df['tm'] != 'none']
-print('new', results_df)
 # Get a rank value fo each dimension based on the approach with the maximal performance
 results_df['ranked'] = results_df.groupby('dim')['mantel_correlation'].rank(method = 'max', ascending=False)
-print('watch2', results_df)
 
 # Group by 'tm' and 'prepro' and calculate the mean rank
 mean_rank_per_approach = results_df.groupby(['tm', 'prepro'])['ranked'].mean().reset_index()
 
 # Optionally, you can sort the result to see which combination has the best (lowest) mean rank
 mean_rank_per_approach_sorted = mean_rank_per_approach.sort_values(by='ranked', ascending=True)
+
 # Save results
-#mean_rank_per_approach_sorted.to_csv('your/output/path')
+mean_rank_per_approach_sorted.to_csv(rank_functional_output + 'ranking_functional_evaluation.csv', index=False)
 
 # Test Significance between the ranks of the results
 # Wilcoxon test to check if the differences between the approaches are significant
@@ -109,4 +106,4 @@ bonferroni_alpha = 0.05
 wilcoxon_df['bonferroni_corrected_p'] = wilcoxon_df['p_value'] * len(wilcoxon_df)
 wilcoxon_df['significant'] = wilcoxon_df['bonferroni_corrected_p'] < bonferroni_alpha
 # Save the results
-#wilcoxon_df.to_csv('your/output/path')
+wilcoxon_df.to_csv(significance_output + 'functional_evaluation_test_significance.csv', index=False))
